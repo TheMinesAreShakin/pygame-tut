44f50c46f75427e267ff6a9d4a3bbc1557c8bf67
TheMinesAreShakin <85915007+TheMinesAreShakin@users.noreply.github.com>
2022-07-13 19:00:46 -0500

    more code

--- GameLoop.py
+++ GameLoop.py
@@ -1,5 +1,6 @@
 #import pygame
 import pygame
+import random
 
 #Importing pygame.locals move the namespace of the constants
 #to the global namespace instead of the pygame namespace so
@@ -16,25 +17,15 @@ from pygame.locals import (
 )
 
 
-def render():
-    screen.fill((0, 0, 0))
-
-    screen.blit(player.surf, player.rect)
-
-    pygame.display.flip()
-
-
-def update():
-    pressedKeys = pygame.key.get_pressed()
-    player.update(pressedKeys)
-
-
 class Player(pygame.sprite.Sprite):
+
+
     def __init__(self):
         super(Player, self).__init__()
         self.surf = pygame.Surface((75,75))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+
 
     def update(self, pressedKeys):
         if pressedKeys[K_UP]:
@@ -56,6 +47,55 @@ class Player(pygame.sprite.Sprite):
             self.rect.bottom = SCREEN_HEIGHT
 
 
+class Enemy(pygame.sprite.Sprite):
+
+
+    def __init__(self):
+        super(Enemy, self).__init__(self)
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 0, 0))
+        self.rect = self.surf.get_rect(
+            center = (
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            #.kill removes the sprite from every group to which it belongs
+            self.kill()
+
+
+def render():
+    #fill background to prevent it from being a drawing game
+    screen.fill((0, 0, 0))
+
+    #blit each sprite to the screen surface using the allSprites group
+    for entity in allSprites:
+        screen.blit(entity.surf, entity.rect)
+
+    #update the display
+    pygame.display.flip()
+
+
+def update():
+    pressedKeys = pygame.key.get_pressed()
+    player.update(pressedKeys)
+
+    # update is a method on the Sprite group that calls the update
+    # method for all members of the group
+    enemies.update()
+
+def add_enemy():
+    new_enemy = Enemy()
+    enemies.add(new_enemy)
+    allSprites.add(new_enemy)
+
+
 #initialize all the pygame modules
 pygame.init()
 
@@ -67,7 +107,19 @@ SCREEN_HEIGHT = 600
 #screen is a surface that represents the inside dimensions of the window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+#create custom event to add new enemy (first line described in notes)
+EVENT_ADD_ENEMY = pygame.USEREVENT + 1
+#set timer takes an event to repeat on an interval in miliseconds
+# in this case it will trigger EVENT_ADD_ENEMY every 250 ms
+pygame.time.set_timer(EVENT_ADD_ENEMY, 250)
+
+#create player
 player = Player()
+
+#create sprite groups
+enemies = pygame.sprite.Group()
+allSprites = pygame.sprite.Group()
+allSprites.add(player)
 
 #game loop var
 running = True
@@ -87,6 +139,10 @@ while running:
         #did the user click the close button?
         elif event.type == QUIT:
             running = False
+
+        # enemy event handler
+        elif event.type == EVENT_ADD_ENEMY:
+            add_enemy()
 
     update()
     render()
